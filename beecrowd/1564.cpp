99f2edabc86172a6868f7a3c8d60e8bb5f6b2694
FELLIPE MARTINS VALLADARES <202401000991@alunos.ibmec.edu.br>
2024-08-12 09:31:26 -0300

    1564 beecrowd

---
+++ beecrowd/1564.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+ 
+    
+    int N;
+    while (cin >> N){
+    
+    if (0 <= N && N <= 100){
+      if (N == 0){
+        cout << "vai ter copa!" << endl;
+      }
+
+      else if (N != 0){
+        cout << "vai ter duas!" << endl;
+      }
+      else {
+        break;
+      }
+    }
+    else
+      break;
+    }
+    
+  
+  
+
+  return 0;
+}
