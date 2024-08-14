9f85a206c2eb7dcb7976336878658bbadc228b34
Fellipe Valladares <120605136+FellipeV540@users.noreply.github.com>
2024-08-14 09:53:17 -0300

    Update 1180.cpp

--- beecrowd/1180.cpp
+++ beecrowd/1180.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int N, menor, pos;
+    cin >> N;
+    int X[N];
+    for (int i = 0; i < N; i++) {
+        cin >> X[i];
+    }
+    for (int j = 0; j < N; j++) {
+        if (j == 0) {
+            menor = X[j];
+            pos = j;
+        }
+        else {
+            if (X[j] < menor) {
+                menor = X[j];
+                pos = j;
+            }
+        }
+    }
+    cout << "Menor valor: " << menor << endl;
+    cout << "Posicao: " << pos << endl;
+    return 0;
+}
