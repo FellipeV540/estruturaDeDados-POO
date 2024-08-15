2331058c44a49822070b96032b3588667834a6db
Fellipe Valladares <120605136+FellipeV540@users.noreply.github.com>
2024-08-15 10:09:38 -0300

    Update 1149.cpp

--- beecrowd/1149/1149.cpp
+++ beecrowd/1149/1149.cpp
@@ -3,23 +3,18 @@ using namespace std;
 
 int main() {
     int A, N;
-
-    // Ler os valores de A e N
     cin >> A >> N;
 
-    // Enquanto N for menor ou igual a zero, continue lendo novos valores para N
     while (N <= 0) {
         cin >> N;
     }
 
-    // Calcular a soma de A + i para i de 0 a N-1
-    int soma = 0;
+    int total = 0;
     for (int i = 0; i < N; i++) {
-        soma += A + i;
+        total += A + i;
     }
-
-    // Imprimir o resultado
-    cout << soma << endl;
+    
+    cout << total << endl;
 
     return 0;
 }
