b10284a0ccd538472d1566b102b37ca6205644f5
Fellipe Valladares <120605136+FellipeV540@users.noreply.github.com>
2024-08-16 10:11:02 -0300

    Create 1865.cpp

---
+++ beecrowd/1865/1865.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int C, N;
+  string nome;
+  cin >> C;
+  for (int i = 0; i < C; i++) {
+    cin >> nome >> N;
+    if (nome == "Thor") {
+      cout << "Y" << endl;
+    }
+    else {
+      cout << "N" << endl;
+    }
+  }
+  
+
+  return 0;
+}
