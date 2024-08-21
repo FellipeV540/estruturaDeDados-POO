df86f291465d5d0605eb411f446f9bfd2fd322e4
Fellipe Valladares <120605136+FellipeV540@users.noreply.github.com>
2024-08-21 09:39:45 -0300

    Create 1864.cpp

---
+++ beecrowd/1864/1864.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+
+  //l i f e  i s  n o t  a  p r o b l e m  t o  b e  s o l v e d
+  string s[34];
+  s[0] = 'L';
+  s[1] = 'I';
+  s[2] = 'F';
+  s[3] = 'E';
+  s[4] = ' ';
+  s[5] = 'I';
+  s[6] = 'S';
+  s[7] = ' ';
+  s[8] = 'N';
+  s[9] = 'O';
+  s[10] = 'T';
+  s[11] = ' ';
+  s[12] = 'A';
+  s[13] = ' ';
+  s[14] = 'P';
+  s[15] = 'R';
+  s[16] = 'O';
+  s[17] = 'B';
+  s[18] = 'L';
+  s[19] = 'E';
+  s[20] = 'M';
+  s[21] = ' ';
+  s[22] = 'T';
+  s[23] = 'O';
+  s[24] = ' ';
+  s[25] = 'B';
+  s[26] = 'E';
+  s[27] = ' ';
+  s[28] = 'S';
+  s[29] = 'O';
+  s[30] = 'L';
+  s[31] = 'V';
+  s[32] = 'E';
+  s[33] = 'D';
+
+  for (int i = 0; i < N; i++) {
+    cout << s[i];
+  }
+  cout << endl;
+
+    return 0;
+}
