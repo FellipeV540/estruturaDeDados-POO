29bf59f09da37ee7e97ee2ed922ec6aad4d3597b
Fellipe Valladares <120605136+FellipeV540@users.noreply.github.com>
2024-08-22 11:29:01 -0300

    Create 2581.cpp

---
+++ beecrowd/2581/2581.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int N;
+  string perguntas;
+  cin >> N;
+
+  for (int i = 0; i < N; i++){
+    cin >> perguntas;
+    cout << "I am Toorg!" << endl;
+  }
+
+    return 0;
+}
